9d74ede9110827a5e1e20b01bab333f3e1994062
Blexyel <passihd@phoenixts.eu>
2026-04-12 03:02:48 +0200

    fix: properly register keybinds
    Signed-off-by: Blexyel <passihd@phoenixts.eu>

--- src/main/java/wtf/blexyel/simpleCameraTweaks/SimpleCameraTweaks.java
+++ src/main/java/wtf/blexyel/simpleCameraTweaks/SimpleCameraTweaks.java
@@ -5,8 +5,9 @@ import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import wtf.blexyel.simpleCameraTweaks.config.Config;
-import wtf.blexyel.simpleCameraTweaks.keybindings.KeybindHelper;
+import wtf.blexyel.simpleCameraTweaks.keybindings.KeyBindings;
 import wtf.blexyel.simpleCameraTweaks.util.FreelookUtils;
+import wtf.blexyel.simpleCameraTweaks.util.Zoom;
 
 public final class SimpleCameraTweaks implements ModInitializer {
 
@@ -16,11 +17,18 @@ public final class SimpleCameraTweaks implements ModInitializer {
 
   public void onInitialize() {
     Config.load();
-    KeybindHelper.load();
+    KeyBindings.init();
 
     ClientTickEvents.END_CLIENT_TICK.register(
         (client) -> {
           FreelookUtils.tick();
+
+          FreelookUtils.active = KeyBindings.FREELOOK_KEY.isDown();
+          if (KeyBindings.OFFHAND_KEY.isDown()) {
+            Config.offhand = !Config.offhand;
+            Config.save();
+          }
+          Zoom.isZoomin = KeyBindings.ZOOM_KEY.isDown();
         });
   }
 }
