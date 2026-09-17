3fbd0a7d43bf3f55b0e8efae18a3446dfecae7af
Blexyel <passihd@phoenixts.eu>
2026-09-18 00:32:57 +0200

    idfk

--- src/main/java/wtf/blexyel/simpleCameraTweaks/mixin/CameraMixin.java
+++ src/main/java/wtf/blexyel/simpleCameraTweaks/mixin/CameraMixin.java
@@ -10,6 +10,7 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.At.Shift;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
@@ -48,9 +49,15 @@ public abstract class CameraMixin {
     cir.setReturnValue(targetFov);
   }
 
-  @Inject(method = "move", at = @At("HEAD"))
+  @Inject(
+      method = "alignWithEntity",
+      at =
+          @At(
+              value = "INVOKE",
+              target = "Lnet/minecraft/client/Camera;setRotation(FF)V",
+              shift = Shift.AFTER))
   // @Inject(method = "update", at = @At("TAIL"))
-  public void setup(float forwards, float up, float right, CallbackInfo ci) {
+  public void setup(float partialTicks, CallbackInfo ci) {
     if (!(entity instanceof Player)) return;
 
     if (FreelookUtils.active) {
